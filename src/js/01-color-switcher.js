e935929c523b92b759bd94599259f92537f5b7d7
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-08-31 21:54:06 +0300

    add logic to color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+buttonStart.addEventListener('click', startChangeColor);
+buttonStop.addEventListener('click', stopChangeColor);
+let intervalStart;
+
+function startChangeColor() {
+  intervalStart = setInterval(() => {
+    buttonStart.setAttribute('disabled', '');
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopChangeColor() {
+  clearInterval(intervalStart);
+  buttonStart.removeAttribute('disabled');
+}
