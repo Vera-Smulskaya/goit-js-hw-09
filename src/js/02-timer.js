ac95015ba8f7181a4bf5cc89398542b7550d6edc
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-09-04 17:31:12 +0300

    add logic to timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,35 +1,12 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
 const buttonStart = document.querySelector('button[data-start]');
 const timePicker = document.getElementById('datetime-picker');
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
-
-buttonStart.addEventListener('click', startTimer);
-
-function startTimer() {}
 
 const options = {
   enableTime: true,
@@ -41,4 +18,43 @@ const options = {
   },
 };
 
-flatpickr(element, options);
+let formatDate = null;
+let deltaTime = null;
+let timerId = null;
+
+flatpickr(timePicker, options);
+buttonStart.addEventListener('click', startTimer);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function pad(value) {
+  return String(value).padStart('2', '0');
+}
+
+function startTimer() {
+  timerId = setInterval(() => {}, 1000);
+}
+
+function updateTimerface(formatDate) {
+  days.textContent = formatDate.days;
+  hours.textContent = formatDate.hours;
+  minutes.textContent = formatDate.minutes;
+  seconds.textContent = formatDate.seconds;
+}
